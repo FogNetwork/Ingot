416e705474ca15204c4b49af468ebc1905423b85
Nebelung <81875430+Nebelung-Dev@users.noreply.github.com>
2022-09-25 09:48:35 -0700

    Updated version

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:(function () {var a = document.createElement('script');a.src = 'https://cdn.jsdelivr.net/gh/FogNetwork/Ingot/ingot.min.js';document.body.appendChild(a);}())
+!function(){var t=document.createElement("script");t.src="https://cdn.jsdelivr.net/gh/FogNetwork/Ingot@latest/ingot.min.js",document.body.appendChild(t)}();
